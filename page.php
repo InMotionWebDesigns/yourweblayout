d046caa6e10aa7830f66cb32ce753c7db2a0e4c5
Brandy Fetzner <brande24@brandedbydesign.net>
2019-06-18 16:22:31 -0400

    Updated page.php

--- page.php
+++ page.php
@@ -11,30 +11,31 @@
  */
 
 get_header(); ?>
+</div><!-- end container -->
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main">
-
-			<div class="row">
-				<div class="col-md-12">
-					<?php while ( have_posts() ) : the_post(); ?>
-						<?php get_template_part( 'content', 'page' ); ?>
-					<?php endwhile; // end of the loop. ?>
-				</div><!-- .col -->
-			</div><!-- .row -->
-
-			<div class="row">
-				<div class="col-md-12">
-					<?php
-						// If comments are open or we have at least one comment, load up the comment template
-						if ( comments_open() || '0' != get_comments_number() ) :
-							comments_template();
-						endif;
-					?>	
-				</div><!-- .col -->
-			</div><!-- .row -->
-
+			<div class="container">
+				<div class="row">
+					<div class="col-md-12">
+						<?php while ( have_posts() ) : the_post(); ?>
+							<?php get_template_part( 'content', 'page' ); ?>
+						<?php endwhile; // end of the loop. ?>
+					</div><!-- .col -->
+				</div><!-- .row -->
+				<div class="row">
+					<div class="col-md-12">
+						<?php
+							// If comments are open or we have at least one comment, load up the comment template
+							if ( comments_open() || '0' != get_comments_number() ) :
+								comments_template();
+							endif;
+						?>	
+					</div><!-- .col -->
+				</div><!-- .row -->
+			</div><!-- end container -->
 		</main><!-- #main -->
 	</div><!-- #primary -->
 
+<div class="container">
 <?php get_footer(); ?>
