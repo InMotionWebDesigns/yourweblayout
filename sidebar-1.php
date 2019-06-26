52ad1ebc9df7909ca4089663a37f0d388b0cad78
Brandy Fetzner <brande24@brandedbydesign.net>
2019-06-26 17:04:27 -0400

    Updated sidebar-1.php

--- sidebar-1.php
+++ sidebar-1.php
@@ -6,8 +6,7 @@
  */
 ?>
 
-	<div id="left-sidebar" class="widget-area" role="complementary">
-		<?php if ( ! dynamic_sidebar( 'sidebar-1' ) ) : ?>
-
-		<?php endif; // end sidebar widget area ?>
-	</div><!-- #left-sidebar -->
+<div id="left-sidebar" class="widget-area" role="complementary">
+	<?php if ( ! dynamic_sidebar( 'sidebar-1' ) ) : ?>
+	<?php endif; // end sidebar widget area ?>
+</div><!-- #left-sidebar -->
