2782f9c652d30ab5ecdb1844dfc8ae6ae16a440d
Brandy Fetzner <brande24@brandedbydesign.net>
2019-06-27 10:19:23 -0400

    Updated page-two-sidebars.php

--- page-two-sidebars.php
+++ page-two-sidebars.php
@@ -6,27 +6,29 @@
  */
 
 get_header(); ?>
+</div><!-- end container -->
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-3">
+						<?php get_sidebar(1); ?>
+					</div><!-- .col -->
 
-			<div class="row">
-				<div class="col-md-3">
-					<?php get_sidebar(1); ?>
-				</div><!-- .col -->
+					<div class="col-md-6">
+						<?php while ( have_posts() ) : the_post(); ?>
+							<?php get_template_part( 'content', 'page' ); ?>
+						<?php endwhile; // end of the loop. ?>
+					</div><!-- .col -->
 
-				<div class="col-md-6">
-					<?php while ( have_posts() ) : the_post(); ?>
-						<?php get_template_part( 'content', 'page' ); ?>
-					<?php endwhile; // end of the loop. ?>
-				</div><!-- .col -->
-
-				<div class="col-md-3">
-					<?php get_sidebar(2); ?>
-				</div><!-- .col -->
-			</div><!-- .row -->
-
+					<div class="col-md-3">
+						<?php get_sidebar(2); ?>
+					</div><!-- .col -->
+				</div><!-- .row -->
+			</div><!-- end container -->
 		</main><!-- #main -->
 	</div><!-- #primary -->
 
+<div class="container">
 <?php get_footer(); ?>
