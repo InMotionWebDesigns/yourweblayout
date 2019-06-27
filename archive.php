9fd074aaea3fd0c000664668bd65fa7a759f5456
Brandy Fetzner <brande24@brandedbydesign.net>
2019-06-27 09:55:50 -0400

    Updated archive.php

--- archive.php
+++ archive.php
@@ -14,10 +14,10 @@ get_header(); ?>
 		<main id="main" class="site-main">
 			<div class="container">	
 				<div class="row">
-					<div class="col-md-3">
+					<div class="col-sm-4 col-md-3">
 						<?php get_sidebar(1); ?>
 					</div><!-- .col -->
-					<div class="col-md-9">
+					<div class="col-sm-8 col-md-9">
 						<?php if ( have_posts() ) : ?>
 							<header class="page-header">
 								<h1 class="page-title">
