d6f6941347a375c6fa84657aefd3057fd51997e7
Brandy Fetzner <brande24@brandedbydesign.net>
2019-06-27 10:28:24 -0400

    Updated page.php

--- page.php
+++ page.php
@@ -17,14 +17,14 @@ get_header(); ?>
 		<main id="main" class="site-main">
 			<div class="container">
 				<div class="row">
-					<div class="col-md-12">
+					<div class="col-sm-12">
 						<?php while ( have_posts() ) : the_post(); ?>
 							<?php get_template_part( 'content', 'page' ); ?>
 						<?php endwhile; // end of the loop. ?>
 					</div><!-- .col -->
 				</div><!-- .row -->
 				<div class="row">
-					<div class="col-md-12">
+					<div class="col-sm-12">
 						<?php
 							// If comments are open or we have at least one comment, load up the comment template
 							if ( comments_open() || '0' != get_comments_number() ) :
