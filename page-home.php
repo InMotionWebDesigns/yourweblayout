0bca6e9b6c1e192a2c7808052925462154ac67a3
Brandy Fetzner <brande24@brandedbydesign.net>
2019-06-27 10:27:30 -0400

    Updated page-home.php

--- page-home.php
+++ page-home.php
@@ -12,7 +12,7 @@ get_header(); ?>
 		<main id="main" class="site-main">
 			<div class="container">
 				<div class="row">
-					<div class="col-md-12">
+					<div class="col-sm-12">
 						<?php while ( have_posts() ) : the_post(); ?>
 							<?php get_template_part( 'content', 'page' ); ?>
 						<?php endwhile; // end of the loop. ?>
