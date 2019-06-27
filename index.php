14921c0ec6969abe095b4b8a6560e66d14046b58
Brandy Fetzner <brande24@brandedbydesign.net>
2019-06-27 09:57:44 -0400

    Updated index.php

--- index.php
+++ index.php
@@ -12,47 +12,36 @@
  */
 
 get_header(); ?>
+</div><!-- end container -->
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main">
-<div class="row">
-
-				<div class="col-md-3">
-				<?php get_sidebar(1); ?>
-
-				</div><!-- .col -->
-
-
-
-				<div class="col-md-9">
-
-		<?php if ( have_posts() ) : ?>
-
-			<?php /* Start the Loop */ ?>
-			<?php while ( have_posts() ) : the_post(); ?>
-
-				<?php
-					/* Include the Post-Format-specific template for the content.
-					 * If you want to override this in a child theme, then include a file
-					 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
-					 */
-					get_template_part( 'content', get_post_format() );
-				?>
-
-			<?php endwhile; ?>
-
-			<?php yourweblayout_paging_nav(); ?>
-
-		<?php else : ?>
-
-			<?php get_template_part( 'content', 'none' ); ?>
-
-		<?php endif; ?>
-</div><!-- .col -->
-
-			</div><!-- .row -->
-
+			<div class="container">
+				<div class="row">
+					<div class="col-sm-4 col-md-3">
+						<?php get_sidebar(1); ?>
+					</div><!-- .col -->
+					<div class="col-sm-8 col-md-9">
+						<?php if ( have_posts() ) : ?>
+							<?php /* Start the Loop */ ?>
+							<?php while ( have_posts() ) : the_post(); ?>
+								<?php
+									/* Include the Post-Format-specific template for the content.
+									 * If you want to override this in a child theme, then include a file
+									 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
+									 */
+									get_template_part( 'content', get_post_format() );
+								?>
+							<?php endwhile; ?>
+							<?php yourweblayout_paging_nav(); ?>
+						<?php else : ?>
+							<?php get_template_part( 'content', 'none' ); ?>
+						<?php endif; ?>
+					</div><!-- .col -->
+				</div><!-- .row -->
+			</div><!-- end container -->
 		</main><!-- #main -->
 	</div><!-- #primary -->
 
+<div class="container">
 <?php get_footer(); ?>
