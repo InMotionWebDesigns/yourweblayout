e497241c7b668ebbb6e534b484ed5cf6c82245da
Brandy Fetzner <brande24@brandedbydesign.net>
2019-06-28 16:08:18 -0400

    Updated archive.php

--- archive.php
+++ archive.php
@@ -8,7 +8,7 @@
  */
 
 get_header(); ?>
-</div><!-- end container -->
+</div><!-- .container -->
 
 	<section id="primary" class="content-area">
 		<main id="main" class="site-main">
@@ -99,7 +99,7 @@ get_header(); ?>
 						<?php endif; ?>
 					</div><!-- .col -->
 				</div><!-- .row -->
-			</div><!-- end container -->
+			</div><!-- .container -->
 		</main><!-- #main -->
 	</section><!-- #primary -->
 
