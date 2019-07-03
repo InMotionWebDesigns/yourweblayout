6e026535cf9c491048798803e5f1cd353eb499a4
Brandy Fetzner <brande24@brandedbydesign.net>
2019-07-03 09:00:43 -0400

    Updated page-left-sidebar.php

--- page-left-sidebar.php
+++ page-left-sidebar.php
@@ -6,23 +6,23 @@
  */
 
 get_header(); ?>
-</div><!-- end container -->
+</div><!-- .container -->
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main">
 			<div class="container">
 				<div class="row">
-					<div class="col-md-3">
+					<div class="col-sm-4 col-md-3">
 						<?php get_sidebar(1); ?>
 					</div><!-- .col -->
 
-					<div class="col-md-9">
+					<div class="col-sm-8 col-md-9">
 						<?php while ( have_posts() ) : the_post(); ?>
 							<?php get_template_part( 'content', 'page' ); ?>
 						<?php endwhile; // end of the loop. ?>
 					</div><!-- .col -->
 				</div><!-- .row -->
-			</div><!-- end container -->
+			</div><!-- .container -->
 		</main><!-- #main -->
 	</div><!-- #primary -->
 
