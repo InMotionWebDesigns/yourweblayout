ae745985b794683c73bc6e70a93ffb7648681e89
Brandy Fetzner <brande24@brandedbydesign.net>
2019-07-03 09:06:47 -0400

    Updated page-two-sidebars.php

--- page-two-sidebars.php
+++ page-two-sidebars.php
@@ -6,27 +6,27 @@
  */
 
 get_header(); ?>
-</div><!-- end container -->
+</div><!-- .container -->
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main">
 			<div class="container">
 				<div class="row">
-					<div class="col-md-3">
+					<div class="col-sm-3">
 						<?php get_sidebar(1); ?>
 					</div><!-- .col -->
 
-					<div class="col-md-6">
+					<div class="col-sm-6">
 						<?php while ( have_posts() ) : the_post(); ?>
 							<?php get_template_part( 'content', 'page' ); ?>
 						<?php endwhile; // end of the loop. ?>
 					</div><!-- .col -->
 
-					<div class="col-md-3">
+					<div class="col-sm-3">
 						<?php get_sidebar(2); ?>
 					</div><!-- .col -->
 				</div><!-- .row -->
-			</div><!-- end container -->
+			</div><!-- .container -->
 		</main><!-- #main -->
 	</div><!-- #primary -->
 
