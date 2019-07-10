5678aa0b10a996e1e0e5edde9642e44c9851279d
Brandy Fetzner <brande24@brandedbydesign.net>
2019-07-10 14:34:49 -0400

    Updated page-two-sidebars.php

--- page-two-sidebars.php
+++ page-two-sidebars.php
@@ -7,7 +7,17 @@
 
 get_header(); ?>
 </div><!-- .container -->
+<?php
+    if(has_post_thumbnail()){
+        echo '<div class="leaderboard-image">';
+        the_post_thumbnail('full');
+        echo '</div>';
+    }
 
+    else{
+        echo '<div class="leaderboard-without-image"></div>';
+    }
+?>
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main">
 			<div class="container">
@@ -15,13 +25,11 @@ get_header(); ?>
 					<div class="col-sm-3">
 						<?php get_sidebar(1); ?>
 					</div><!-- .col -->
-
 					<div class="col-sm-6">
 						<?php while ( have_posts() ) : the_post(); ?>
 							<?php get_template_part( 'content', 'page' ); ?>
 						<?php endwhile; // end of the loop. ?>
 					</div><!-- .col -->
-
 					<div class="col-sm-3">
 						<?php get_sidebar(2); ?>
 					</div><!-- .col -->
