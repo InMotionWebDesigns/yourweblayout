7120ade64c56ac7b085b572ecffd2ecfeb5f2ec9
Brandy Fetzner <brande24@brandedbydesign.net>
2019-07-10 14:34:05 -0400

    Updated page-right-sidebar.php

--- page-right-sidebar.php
+++ page-right-sidebar.php
@@ -7,7 +7,17 @@
 
 get_header(); ?>
 </div><!-- .container -->
+<?php
+    if(has_post_thumbnail()){
+        echo '<div class="leaderboard-image">';
+        the_post_thumbnail('full');
+        echo '</div>';
+    }
 
+    else{
+        echo '<div class="leaderboard-without-image"></div>';
+    }
+?>
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main">
 			<div class="container">
@@ -17,7 +27,6 @@ get_header(); ?>
 							<?php get_template_part( 'content', 'page' ); ?>
 						<?php endwhile; // end of the loop. ?>
 					</div><!-- .col -->
-
 					<div class="col-sm-4 col-md-3">
 						<?php get_sidebar(2); ?>
 					</div><!-- .col -->	
